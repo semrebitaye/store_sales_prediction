35a02c37285405463b40939e4f5b61d4dc577518
semrebitaye <asimrobitaye23@gmail.com>
2024-09-24 21:53:33 +0300

    sales prediction further analysis

--- scripts/sales_analysis.py
+++ scripts/sales_analysis.py
@@ -1,11 +1,24 @@
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
-from statsmodels.tsa.stattools import acf, pacf
-import holidays
 import logging
 
-# configure logging
+# setup logger
+def setup_logger(name, log_file, level=logging.INFO):
+    """Function to setup a logger."""
+    formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+
+    # Create the log directory if it does not exist
+    if not os.path.exists(os.path.dirname(log_file)):
+        os.makedirs(os.path.dirname(log_file))
+
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(formatter)
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
+
+    
